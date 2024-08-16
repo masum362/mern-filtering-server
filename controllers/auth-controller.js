150828128bfe3df29f703ac8836818491652af40
masum362 <masumahmed64077@gmail.com>
2024-08-16 16:00:45 +0600

    added auth-controller

---
+++ controllers/auth-controller.js
@@ -0,0 +1,14 @@
+const login = (req ,res) => {
+    console.log('loggin called');
+};
+
+const register = (req ,res) => { 
+    const user = req.body;
+
+    const isAlreadyRegistered = await
+
+
+};
+
+
+export {login ,register};
