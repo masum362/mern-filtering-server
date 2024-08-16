0a618ddf161a1a4d33f6d2c40c2761e57e3579a7
masum362 <masumahmed64077@gmail.com>
2024-08-16 17:51:21 +0600

    register user api done

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,17 +1,49 @@
 import userModel from "../model/userModel.js";
 
-const login = (req ,res) => {
-    console.log('loggin called');
+const login = async(req, res) => {
+  const user = req.body;
+
+  try {
+    const isAlreadyRegistered = await userModel.findOne({ email: user.email });
+    if (isAlreadyRegistered)
+      return res.json({ success: false, message: "User Already registered " });
+
+    const newUser = await userModel(user);
+    newUser.save();
+    return res.json({
+      success: true,
+      user: newUser,
+      message: "User registration successful",
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+    
 };
 
-const register =async (req ,res) => { 
-    const user = req.body;
+const register = async (req, res) => {
+  const user = req.body;
+  try {
+    const isAlreadyRegistered = await userModel.findOne({ email: user.email });
+    if (isAlreadyRegistered)
+      return res.json({ success: false, message: "User Already registered " });
 
-    const isAlreadyRegistered = await userModel.findOne({email:user.email})
-    if(isAlreadyRegistered) return res.status(404).json({message:"User Already registered "})
-
-
+    const newUser = await userModel(user);
+    newUser.save();
+    return res.json({
+      success: true,
+      user: newUser,
+      message: "User registration successful",
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
-
-export {login ,register};
+export { login, register };
