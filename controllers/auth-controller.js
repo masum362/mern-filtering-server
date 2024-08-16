281b3d699f1f20dadec9b75c0b4b81f9127ade49
masum362 <masumahmed64077@gmail.com>
2024-08-16 17:45:38 +0600

    added get product api

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,11 +1,14 @@
+import userModel from "../model/userModel.js";
+
 const login = (req ,res) => {
     console.log('loggin called');
 };
 
-const register = (req ,res) => { 
+const register =async (req ,res) => { 
     const user = req.body;
 
-    // const isAlreadyRegistered = await
+    const isAlreadyRegistered = await userModel.findOne({email:user.email})
+    if(isAlreadyRegistered) return res.status(404).json({message:"User Already registered "})
 
 
 };
--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,34 +1,57 @@
 import productModel from "../model/productModel.js";
 
-const getProducts = (req, res) => {};
+const getProducts = async (req, res) => {
+  const { search, brand, category, price_range, sort_order } = req.query;
+
+  try {
+    let query = {};
+    if (search) query.name = new RegExp(search, "i");
+    if (brand) query.brand = brand;
+    if (category) query.category = category;
+    if (price_range) {
+      query.price = {
+        $gte: 0,
+        $lte: Number(price_range),
+      };
+    }
+
+    let sortBy = {};
+    if (sort_order === "low_to_high") {
+      sortBy.price = 1;
+    } else if (sort_order === "high_to_low") {
+      sortBy.price = -1;
+    } else if (sort_order === "newest_date") {
+      sortBy.date = -1;
+    }
+
+    const result = await productModel.find(query).sort(sortBy);
+   return res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const addProduct = async (req, res) => {
-//   // name,photoURL,description,price,brand,category,rating
-
-//   const { products } = await fetch(
-//     "https://dummyjson.com/products/category/mobile-accessories"
-//   ).then((res) => res.json());
-
-  
-
-//   const product =products.map(products => ({
-//     name: products.title,
-//     photoURL: products.thumbnail,
-//     description: products.description,
-//     price: Math.round(parseFloat(products.price)),
-//     brand: products.brand,
-//     category: products.category,
-//     rating: parseFloat(products.rating),
-//   }));
-
-//   console.log(product);
-
-//   try {
-//     const result = await productModel.insertMany(product);
-//    return res.send(result);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
+  //   // name,photoURL,description,price,brand,category,rating
+  //   const { products } = await fetch(
+  //     "https://dummyjson.com/products/category/mobile-accessories"
+  //   ).then((res) => res.json());
+  //   const product =products.map(products => ({
+  //     name: products.title,
+  //     photoURL: products.thumbnail,
+  //     description: products.description,
+  //     price: Math.round(parseFloat(products.price)),
+  //     brand: products.brand,
+  //     category: products.category,
+  //     rating: parseFloat(products.rating),
+  //   }));
+  //   console.log(product);
+  //   try {
+  //     const result = await productModel.insertMany(product);
+  //    return res.send(result);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
 };
 
 export { getProducts, addProduct };
