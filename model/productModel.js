0a84899c1ba3be31cd27740014ff174fecb1ca0e
masum362 <masumahmed64077@gmail.com>
2024-08-16 16:02:17 +0600

    added product model

---
+++ model/productModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    photoURL: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productModel = new mongoose.model("user", productSchema);
+
+export default productModel;
