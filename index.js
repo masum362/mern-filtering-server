861f7e0190399ea43fc033eb5edd59caba44af8f
masum362 <masumahmed64077@gmail.com>
2024-08-18 22:48:58 +0600

    cors config update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
     credentials:true,
-    origin:['http://localhost:5173','https://thefind-137e7.web.app']
+    origin:["https://thefind-137e7.web.app","http://localhost:5173"]
 }))
 app.use(express.json())
 app.use('/',router)
